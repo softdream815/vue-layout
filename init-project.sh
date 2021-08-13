28da65c91088dde71b5f77f41dd5e1488536f977
softdream815 <uyg0815@gmail.com>
2021-08-13 10:31:27 +0300

    Обновлен init

--- init-project.sh
+++ init-project.sh
@@ -1,3 +1,20 @@
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
+echo "
+████─████─████───██─███─████─███
+█──█─█──█─█──█────█─█───█──█──█
+████─████─█──█────█─███─█─────█
+█────█─█──█──█─█──█─█───█──█──█
+█────█─█──████─████─███─████──█
+
+
+███─█──█─███─███─███─████─█───███─████─████─███─███─████─█──█
+─█──██─█──█───█───█──█──█─█────█────██─█──█──█───█──█──█─██─█
+─█──█─██──█───█───█──████─█────█───██──████──█───█──█──█─█─██
+─█──█──█──█───█───█──█──█─█────█──██───█──█──█───█──█──█─█──█
+███─█──█─███──█──███─█──█─███─███─████─█──█──█──███─████─█──█
+"
+
 # Удаляем информацию о репозитории проекта vue-project-layout
 rm -Rf .git;
 
@@ -9,20 +26,28 @@ rm README.md
 touch README.md
 
 # Инициализируем git
-git init;
-git add . ;
-git commit -m "Init project";
-
-git branch -M main; # Создаем ветку main
-git checkout -b dev; # Создаем ветку dev
-git checkout -b build; # Создаем ветку build
+git init
+git add . 
+git commit -m "Init project"
+git branch -M main
+git checkout -b dev
+git checkout -b build
 
 # Если есть в параметрах ссылка на github репозитрий - инициализирует удаленный репозиторий  и пушим на github
 if [[ $1 ]]; then
-  git remote add origin $1;
-  git push -u origin main;
-  git push -u origin dev;
-  git push -u origin build;
+  git remote add origin $1
+
+  # Заменить на цикл
+  # BRANCHES=("main" "dev" "build")
+  # for i in ${!BRANCHES[*]}
+  #   do
+  #       git push -u origin ${BRANCHES[$i]} 
+  #   done
+  # fi
+
+  git push -u origin main
+  git push -u origin dev
+  git push -u origin build
 fi
 
 git checkout dev
